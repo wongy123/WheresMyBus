6277c6824e037dde12c007d958b2a575982fef37
wongy123 <wongmanshing0226@gmail.com>
2025-08-22 00:14:30 +1000

    Add getStop services

--- server/test.js
+++ server/test.js
@@ -1,15 +1,36 @@
-import { getAllRoutes, getOneRoute} from './src/services/gtfsQueries.js';
+import { getAllRoutes, getOneRoute, getAllStops, getOneStop} from './src/services/gtfsQueries.js';
 
-// Example usage of getAllRoutes
+/* // Example usage of getAllRoutes
 getAllRoutes().then(routes => {
     console.log(routes);
 }).catch(err => {
     console.error('Error fetching all routes:', err);
-});
+}); */
 
-// Example usage of getOneRoute
+/* // Example usage of getOneRoute
 getOneRoute('60').then(route => {
     console.log(route);
 }).catch(err => {
     console.error('Error fetching route 100:', err);
+}); */
+
+// Example usage of getAllStops
+getAllStops().then(stops => {
+    console.log(stops);
+}).catch(err => {
+    console.error('Error fetching all stops:', err);
+}); 
+
+// Example usage of getOneStop
+getOneStop('999').then(stop => {
+    console.log(stop);
+}).catch(err => {
+    console.error('Error fetching stop:', err);
+});
+
+// Example usage of getOneStop
+getOneStop('place_qsbs').then(stop => {
+    console.log(stop);
+}).catch(err => {
+    console.error('Error fetching stop:', err);
 });
