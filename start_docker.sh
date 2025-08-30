1e5a5ed7135a9ea88f43eaae8e44b9f8c5d379bc
Angus Wong <158044675+wongy123@users.noreply.github.com>
2025-08-30 22:39:49 +1000

    Update start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,3 +1,5 @@
+docker pull 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/n11941073/assessment1/api:latest
+docker pull 901444280953.dkr.ecr.ap-southeast-2.amazonaws.com/n11941073/assessment1/client:latest
 sudo mkdir -p uploads db-data
 sudo chown -R 1000:1000 uploads
 sudo chmod -R 775 uploads
