4b7c4249f86da10d88c953d65f817e27f2a0a72c
wongy123 <wongmanshing0226@gmail.com>
2025-09-25 01:35:50 +1000

    Add MFA via email

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 // src/routes/auth.routes.js
 import { Router } from 'express';
-import { register, confirm, login } from '../controllers/auth.controller.js';
+import { register, confirm, login, loginMfa } from '../controllers/auth.controller.js';
 import { cognitoLogin, cognitoCallback, cognitoRedeem } from '../controllers/cognito.controller.js';
 
 const router = Router();
@@ -8,6 +8,7 @@ const router = Router();
 router.post('/register', register);
 router.post('/confirm', confirm);
 router.post('/login', login);
+router.post('/login/mfa', loginMfa);
 
 router.get('/cognito/login', cognitoLogin);
 router.get('/cognito/callback', cognitoCallback);
